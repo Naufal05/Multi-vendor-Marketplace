7838aa49a550ea5dc0013ef0a74f76c5b81fa76b
Mohamed Naufal <mnaufalmz@gmail.com>
2025-09-09 02:07:33 +0530

    error-handle-middleware

--- packages/middleware/error-handler/error-middleware.ts
+++ packages/middleware/error-handler/error-middleware.ts
@@ -1,3 +1,20 @@
+import { AppError } from "./index";
+import {Request, Response} from "express";
+
 export const errorMiddleware = (err:Error, req:Request, res:Response) => {
-    if(err instanceof AppError)
+    if(err instanceof AppError) {
+        console.log(`Error ${req.method} ${req.url} - ${err.message}`);
+
+        return res.status(err.statuscode).json({
+            status: "error",
+            message: err.message,
+            ...(err.details && {details: err.details}), 
+        });
+    }
+
+    console.log("Unhadled Error:", err);
+
+    return res.status(500).json({
+        error: "something went wrong, Please try again!"
+    })
 } 
